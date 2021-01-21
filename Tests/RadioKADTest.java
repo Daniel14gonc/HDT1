8fcb14a2b2bd6e8b3d38668873bf4594009fb32a
KennethGalvez <gal20079@uvg.edu.gt>
2021-01-20 21:43:31 -0600

    Update RadioKADTest.java

--- Tests/RadioKADTest.java
+++ Tests/RadioKADTest.java
@@ -4,6 +4,15 @@ public class RadioKADTest {
 
     @org.junit.Test
     public void isON() {
+        boolean tipo = false;
+        RadioKAD instance = new RadioKAD();
+        boolean expResult = true;
+        boolean result = instance.isON();
+        assertEquals(expResult, result);
+        if (result.equals(expResult)){
+            fail("Fallo el metodo");
+        }
+
     }
 
     @org.junit.Test
