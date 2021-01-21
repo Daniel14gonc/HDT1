2649d5ebc0c3647c6cf06b855e32e21975f1a3d8
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2021-01-20 20:07:30 -0600

    Metodos de radio KAD

--- src/RadioKAD.java
+++ src/RadioKAD.java
@@ -1,3 +1,49 @@
 public class RadioKAD implements Radio{
 
+    private boolean ON;
+
+    private int emisoraActualAM;
+    private double emisoraActualFM;
+
+    private int frecuenciaActual;
+
+    private int[] emisorasAM;
+    private double[] emisorasFM;
+
+
+    public RadioKAD(){
+        //Al iniciar la radio siempre esta apagada.
+        ON = false;
+
+        //Para la frecuencia AM equivale a 0 y FM a 1.
+        frecuenciaActual = 0;
+
+        emisorasAM = new int[12];
+        emisorasFM = new double[12];
+
+
+        //La emisora inicia siempre en la mas baja.
+        emisoraActualAM = 530;
+        emisoraActualFM = 87.9;
+
+    }
+
+    public boolean isON(){
+        return ON;
+    }
+
+    public void encender(){
+
+        ON = true;
+
+    }
+
+    public void apagar(){
+
+        ON = false;
+
+    }
+
+
+
 }
