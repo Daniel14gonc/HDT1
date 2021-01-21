1c81ec3496c3c89cdc2298dac497c7a94d3d565f
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2021-01-20 19:40:47 -0600

    encendido de la radio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Radio radio = (Radio) new RadioKAD();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\n\nBienvenido a la RadioKAD");
+        System.out.print("\t Desea prender la radio? [si/no] ");
+
+        if(scan.next().equals("si")){
+            radio.encender();
+        }else{
+            System.out.println("no");
+        }
+
+
+
 
     }
 }
